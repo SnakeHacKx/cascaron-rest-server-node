773b3828269ee100fe68ec3610463ae57c322f80
SnakeHacKx <67122931+SnakeHacKx@users.noreply.github.com>
2023-03-16 13:42:45 -0500

    Default PORT updated

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 3000;
     this.usersPath = '/api/users';
 
     // Middlewares
